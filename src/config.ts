875c6888f1d998742c418c5fa708dd9a86fc4f13
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2024-01-12 05:53:32 +0530

    wip

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,6 @@ import chokidar from "chokidar";
 export const config = {
   watchDir: "/Users/ashu/dev/ZusBE/gosdk/wasmsdk",
   watcherOptions: { ignored: "*/**/*.wasm" } as chokidar.WatchOptions,
+  webAppsDir: "/Users/ashu/dev/ZusFE/packages",
+  wasmDest: ["/vult/public", "/shared/public"],
 };
--- src/event-action.example.ts
+++ src/event-action.example.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { cp2 } from "./c-promise2";
 import { config } from "./config";
 import { ShellProcess } from "./utils/ShellProcess";
@@ -10,26 +11,44 @@ export const fsEventAction = cp2.promisify(function* ({
   actionId,
   prevId,
 }: TActionParams) {
-
   // @ts-ignore
   const wasmPath: string = yield genWasm(actionId, prevId);
+  console.log(1, wasmPath);
+  yield useWasmInApps(wasmPath);
 });
 
-type TProcessRet<TRet> = Generator<Promise<void>, TRet, unknown>;
-const genWasm = cp2.promisify(function* (
-  actionId: string,
-  prevId: string
-): TProcessRet<string> {
+const genWasm = cp2.promisify(function* (actionId: string, prevId: string) {
   const genCmd = `GOOS=js CGO_ENABLED=0 GOARCH=wasm go build -o zcn-${actionId}.wasm github.com/0chain/gosdk/wasmsdk`;
   const rmCmd = `rm zcn-${prevId}.wasm`;
 
-  const gosdkProcess = new ShellProcess({ cwd: config.watchDir }, "wasm-gen");
-  gosdkProcess.addOr(rmCmd);
-  gosdkProcess.addAnd(genCmd);
+  const genProcess = new ShellProcess({ cwd: config.watchDir }, "wasm-gen");
+  genProcess.orAdd(rmCmd).andAdd(genCmd);
 
-  yield gosdkProcess.exec();
-
+  const a = yield genProcess.exec();
+  console.log(a);
   const wasmPath = `${config.watchDir}/zcn-${actionId}.wasm`;
   logr.ok(`WASM generated: ${wasmPath}`);
   return wasmPath;
+});
+
+const useWasmInApps = cp2.promisify(function* (wasmPath: string) {
+  // replace wasm
+  for (const wasmDest of config.wasmDest) {
+    const wasmDir = path.join(config.webAppsDir, wasmDest);
+    // const findWasmPathsCmd = `find  -name 'zcn*.wasm' -type f`;
+    const findProcess = new ShellProcess({ cwd: wasmDir }, "find-wasm-paths");
+
+    const findWasmPathsCmd = `find . -name 'zcn*.wasm'`; // macos
+    findProcess.andAdd(findWasmPathsCmd);
+    const allWasmPaths = yield findProcess.exec();
+    console.log(333, allWasmPaths);
+    for (const wasmPath of allWasmPaths) {
+      console.log(222, wasmPath);
+    }
+  }
+
+  // code replace
+  // const codeReplaceLn = `sed -i 's/await fetch('\''\/zcn.*\.wasm.*'\'';/await fetch("abcdefg");/' zcn.js`;
+  // const replaceProcess = //
+  //   new ShellProcess({ cwd: config.webAppsDir }, "replace-wasm");
 });
